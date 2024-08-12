f8b66d8e95ca84eac1c8638b8180b3295b3bd629
Nishant <nishant852800@gamil.com>
2024-08-12 20:21:43 +0530

    Updated App.js and components

--- src/App.js
+++ src/App.js
@@ -8,17 +8,21 @@ import Products from "./component/Product";
 import Guidelines from "./component/Guidelines";
 import { Routes, Route } from "react-router-dom";
 import AddProducts from "./component/AddProducts";
-import UpdateProducts from "./component/UpdateProduct";
+import UpdateProduct from "./component/UpdateProduct";
 
 function App() {
   return (
     <>
       <Menu />
       <Routes>
+        {/* Default Route for Landing Page */}
+        <Route path="/" element={<About />} />
+
+        {/* Other Routes */}
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
         <Route path="/Add-Products" element={<AddProducts />} />
-        <Route path="/UpdateProducts/:id" element={<UpdateProducts />} />
+        <Route path="/UpdateProduct/:id" element={<UpdateProduct />} />
         <Route path="/vision" element={<Vision />} />
         <Route path="/support" element={<Support />} />
         <Route path="/guidelines" element={<Guidelines />} />
